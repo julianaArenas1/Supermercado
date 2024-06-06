4df8ac7d8d4c63a0ecd434759885e43da1870614
DarkViper-128 <juanpaflorez77@gmail.com>
2024-06-05 22:06:20 -0500

    Updated part of Interfaces

---
+++ Supermercado/Supermercado/Domain/Interfaces/ISupermercado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Supermercado.Domain.Interfaces
+{
+    public interface ICountryService
+    {
+        //Creación de métodos para productos
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> EditProductAsync(Product product);
+        Task<Product> DeleteProductAsync(Guid id);
+
+        //Creación de métodos para Categorías
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> EditProductAsync(Product product);
+        Task<Product> DeleteProductAsync(Guid id);
+
+        //Creación de métodos para Ventas
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<Product> EditProductAsync(Product product);
+        Task<Product> DeleteProductAsync(Guid id);
+
+    }
+}
